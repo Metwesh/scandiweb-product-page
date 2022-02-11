9d0c832e9b718703d307b9eb18d9b9eb3c89e585
Mohamed H. Aly <94052626+Metwesh@users.noreply.github.com>
2022-02-11 20:00:15 +0200

    Fixed links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import { Route, Routes, useMatch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import AddProduct from "./routes/AddProduct";
 import Error404 from "./components/Error404";
 import Home from "./routes/Home";
 function App() {
-  let { url } = useMatch();
   return (
     <Routes>
-      <Route path={`${url}/`} component={<Home />} />
-      <Route path={`${url}/add-product`} component={<AddProduct />} />
-      <Route path={`${url}/*`} component={<Error404 />} />
+      <Route path="/">
+        <Home />
+      </Route>
+      <Route path="/add-product">
+        <AddProduct />
+      </Route>
+      <Route path="/*">
+        <Error404 />
+      </Route>
     </Routes>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./scss/styles.scss";
 import App from "./App";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Outlet, Route } from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter>
-    <Route path="scandiweb-product-page" component={<App />} />
+    <Route path="scandiweb-product-page">
+      <App />
+      </Route>
+      <Outlet />
   </BrowserRouter>,
   document.getElementById("root")
 );
