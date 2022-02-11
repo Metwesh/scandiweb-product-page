272c8f86b995f418c2849c0c5f1c7d629af932c4
Mohamed H. Aly <94052626+Metwesh@users.noreply.github.com>
2022-02-11 20:33:41 +0200

    FFS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import { Outlet, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import AddProduct from "./routes/AddProduct";
 import Error404 from "./components/Error404";
 import Home from "./routes/Home";
 function App() {
   return (
     <Routes>
-      <Route path="scandiweb-product-page">
         <Route path="/">
           <Home />
         </Route>
@@ -15,8 +14,6 @@ function App() {
         <Route path="/*">
           <Error404 />
         </Route>
-        <Outlet />
-      </Route>
     </Routes>
   );
 }
