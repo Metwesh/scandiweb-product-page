4df5c8a9f517f43dbe7f11514f381f64b4c93ff3
Mohamed H. Aly <94052626+Metwesh@users.noreply.github.com>
2022-02-11 22:36:20 +0200

    Fixed?

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import AddProduct from "./routes/AddProduct";
 import Error404 from "./components/Error404";
 import Home from "./routes/Home";
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" exact element={<Home />}>
-        <Route path="/add-product" exact element={<AddProduct />} />
+    <BrowserRouter basename="/scandiweb-product-page">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add-product" element={<AddProduct />} />
         <Route path="/*" exact element={<Error404 />} />
-      </Route>
-    </Routes>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./scss/styles.scss";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/scandiweb-product-page">
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
