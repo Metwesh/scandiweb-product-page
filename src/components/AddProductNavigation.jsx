28439da7d1694e4e4b47e32160ace134a463a638
Mohamed H. Aly <94052626+Metwesh@users.noreply.github.com>
2022-02-11 22:20:28 +0200

    Fixed navbar

--- src/components/AddProductNavigation.jsx
+++ src/components/AddProductNavigation.jsx
@@ -2,20 +2,22 @@ import { BrowserRouter, Link } from "react-router-dom";
 
 export default function ProductNavigation() {
   return (
-    <BrowserRouter basename="/scandiweb-product-page">
-      <nav>
-        <h1>Add product</h1>
-        <button
-          type="submit"
-          form="product_form"
-          className="button button--primary push">
-          Save
-        </button>
-        <Link className="button button--outline space" to="/">
-          Cancel
-        </Link>
-      </nav>
-      <hr />
-    </BrowserRouter>
+    <>
+      <BrowserRouter basename="/scandiweb-product-page/">
+        <nav>
+          <h1>Add product</h1>
+          <button
+            type="submit"
+            form="product_form"
+            className="button button--primary push">
+            Save
+          </button>
+          <Link className="button button--outline space" to="/">
+            Cancel
+          </Link>
+        </nav>
+        <hr />
+      </BrowserRouter>
+    </>
   );
 }
--- src/components/ProductNavigation.jsx
+++ src/components/ProductNavigation.jsx
@@ -2,20 +2,22 @@ import { BrowserRouter, Link } from "react-router-dom";
 
 export default function ProductNavigation() {
   return (
-    <BrowserRouter basename="/scandiweb-product-page">
-      <nav>
-        <h1>Product list</h1>
-        <Link className="button button--primary push" to="/add-product">
-          Add Product
-        </Link>
-        <button
-          className="button button--outline space"
-          form="delete-product-form"
-          id="delete-product-btn">
-          Mass delete
-        </button>
-      </nav>
-      <hr />
-    </BrowserRouter>
+    <>
+      <BrowserRouter basename="/scandiweb-product-page/">
+        <nav>
+          <h1>Product list</h1>
+          <Link className="button button--primary push" to="/add-product">
+            Add Product
+          </Link>
+          <button
+            className="button button--outline space"
+            form="delete-product-form"
+            id="delete-product-btn">
+            Mass delete
+          </button>
+        </nav>
+        <hr />
+      </BrowserRouter>
+    </>
   );
 }
