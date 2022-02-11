8628a967cca30484721102cab50ff6d6cd561431
Mohamed H. Aly <94052626+Metwesh@users.noreply.github.com>
2022-02-11 20:29:38 +0200

    Fixed links please for the love of god

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import { Route, Routes } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 import AddProduct from "./routes/AddProduct";
 import Error404 from "./components/Error404";
 import Home from "./routes/Home";
 function App() {
   return (
     <Routes>
-      <Route path="/">
-        <Home />
-      </Route>
-      <Route path="/add-product">
-        <AddProduct />
-      </Route>
-      <Route path="/*">
-        <Error404 />
+      <Route path="scandiweb-product-page">
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/add-product">
+          <AddProduct />
+        </Route>
+        <Route path="/*">
+          <Error404 />
+        </Route>
+        <Outlet />
       </Route>
     </Routes>
   );
--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./scss/styles.scss";
 import App from "./App";
-import { BrowserRouter, Outlet, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter>
-    <Route path="scandiweb-product-page">
       <App />
-      </Route>
-      <Outlet />
   </BrowserRouter>,
   document.getElementById("root")
 );
