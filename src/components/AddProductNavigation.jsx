8e6999d6de11766fb76c4ac15b939e54b24220d0
Mohamed H. Aly <94052626+Metwesh@users.noreply.github.com>
2022-02-11 22:22:14 +0200

    Fixed navbar 2

--- src/components/AddProductNavigation.jsx
+++ src/components/AddProductNavigation.jsx
@@ -1,23 +1,21 @@
-import { BrowserRouter, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function ProductNavigation() {
   return (
     <>
-      <BrowserRouter basename="/scandiweb-product-page/">
-        <nav>
-          <h1>Add product</h1>
-          <button
-            type="submit"
-            form="product_form"
-            className="button button--primary push">
-            Save
-          </button>
-          <Link className="button button--outline space" to="/">
-            Cancel
-          </Link>
-        </nav>
-        <hr />
-      </BrowserRouter>
+      <nav>
+        <h1>Add product</h1>
+        <button
+          type="submit"
+          form="product_form"
+          className="button button--primary push">
+          Save
+        </button>
+        <Link className="button button--outline space" to="/">
+          Cancel
+        </Link>
+      </nav>
+      <hr />
     </>
   );
 }
