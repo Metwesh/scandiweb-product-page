508c609eef8bd3ff2ca1cc17b8f60ac0a5ac379d
Mohamed H. Aly <94052626+Metwesh@users.noreply.github.com>
2022-02-14 13:33:35 +0200

    Fixed button spacing

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+
+export default function Navigation() {
+  return window.location.pathname === "/" ? (
+    <>
+      <nav>
+        <h1>Product list</h1>
+        <Link className="button button--primary push" to="/add-product">
+          ADD
+        </Link>
+        <button
+          className="button button--outline space"
+          form="delete-product-form"
+          id="delete-product-btn">
+          MASS DELETE
+        </button>
+      </nav>
+      <hr />
+    </>
+  ) : window.location.pathname === "/add-product" ? (
+    <>
+      <nav>
+        <h1>Add product</h1>
+        <button
+          type="submit"
+          form="product_form"
+          className="button button--primary push">
+          Save
+        </button>
+        <Link className="button button--outline space" to="/">
+          Cancel
+        </Link>
+      </nav>
+      <hr />
+    </>
+  ) : null;
+}
