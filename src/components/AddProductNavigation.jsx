b7631213b026b0e89199d7d7f53e3914c988f279
Mohamed H. Aly <94052626+Metwesh@users.noreply.github.com>
2022-02-14 12:55:03 +0200

    Split Nav for test 2

--- src/components/AddProductNavigation.jsx
+++ src/components/AddProductNavigation.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-export default function ProductNavigation() {
+export default function AddProductNavigation() {
   <>
     <nav>
       <h1>Add product</h1>
