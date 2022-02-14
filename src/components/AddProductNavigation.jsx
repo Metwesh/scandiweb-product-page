9451ebe104db35c2311687db96dcb0b34a4f8316
Mohamed H. Aly <94052626+Metwesh@users.noreply.github.com>
2022-02-14 13:00:33 +0200

    Fixed return on nav

--- src/components/AddProductNavigation.jsx
+++ src/components/AddProductNavigation.jsx
@@ -1,19 +1,21 @@
 import { Link } from "react-router-dom";
 
 export default function AddProductNavigation() {
-  <>
-    <nav>
-      <h1>Add product</h1>
-      <button
-        type="submit"
-        form="product_form"
-        className="button button--primary push">
-        Save
-      </button>
-      <Link className="button button--outline space" to="/">
-        Cancel
-      </Link>
-    </nav>
-    <hr />
-  </>;
+  return (
+    <>
+      <nav>
+        <h1>Add product</h1>
+        <button
+          type="submit"
+          form="product_form"
+          className="button button--primary push">
+          Save
+        </button>
+        <Link className="button button--outline space" to="/">
+          Cancel
+        </Link>
+      </nav>
+      <hr />
+    </>
+  );
 }
